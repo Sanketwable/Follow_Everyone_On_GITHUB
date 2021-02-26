a6900bff07cdf71ca4ab35d1db09f50ecc94b4be
Wable Sanket <sanketwable123@gmail.com>
2021-02-26 09:13:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -28,11 +28,11 @@ type Response struct {
 func main() {
 	from := 628740
 	for {
-		time.Sleep(5 * time.Second) // github detects the unusuall traffic from particular ip so having delay will not stop api response 
-		from := 628740
+
 		res := getuser(from)
 		for _, j := range res {
 			follow(j.Name)
+			time.Sleep(5 * time.Second) 	 // github detects the unusuall traffic from particular ip so having delay will not stop api response 
 			from = j.ID
 			fmt.Println("followed : ", j.Name)
 		}
